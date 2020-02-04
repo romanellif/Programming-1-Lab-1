172a0937aad1bf90ee143ffcfece291690eabbe3
romanellif <60394122+romanellif@users.noreply.github.com>
2020-02-04 09:09:21 -0500

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World Practice 1");
+   		
+   		int year;
+   		double distance;
+   		char letter;
+   		boolean state;
+
+   		year = 2020;
+   		nextYear = year + 1;
+   		System.out.println("The current year is"+ year);
+   		System.out.println("The next year will be" + nextYear);
+   		distance = 2.3;
+   		letter = 'a';
+   		state = true;
+    }
+
+}
